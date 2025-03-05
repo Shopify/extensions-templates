ecc44d7070144a19e84f3274de03fe37357f5a4d
Trish Ta <trish.ta@gmail.com>
2025-03-05 12:05:47 -0500

    Update Admin templates for remote-dom

--- conditional-action-extension-ts/src/condition/shouldRender.ts
+++ conditional-action-extension-ts/src/condition/shouldRender.ts
@@ -1,15 +1,12 @@
-/// <reference types="@shopify/ui-extensions/admin" />
-
-// The target used here must match the target used in the extension's toml file (./shopify.extension.toml), 
-// except for the "should-render" suffix
-const TARGET = 'admin.product-details.action.should-render';
+/// <reference types="@shopify/ui-extensions/admin.product-details.action.should-render" />
 
 // The second argument to the render callback provides access to the resource ID.
-export default shopify.extend(TARGET, async ({ data }) => {
+export default async function extension() {
+  const {data} = shopify;
   const variantCount = await getVariantsCount(data.selected[0].id);
 
   return {display: variantCount > 1 }
-});
+};
 
 // Use direct API calls to fetch data from Shopify.
 // See https://shopify.dev/docs/api/admin-graphql for more information about Shopify's GraphQL API
