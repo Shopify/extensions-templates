ed4acd5ffc46ba69047117abab112b13364c719d
Tanusri Harish <tanusri.harish@shopify.com>
2025-07-07 13:20:11 -0400

    fixing rust lint error

--- functions-cart-transform-rs/src/cart_transform_run.rs
+++ functions-cart-transform-rs/src/cart_transform_run.rs
@@ -3,7 +3,10 @@ use shopify_function::prelude::*;
 use shopify_function::Result;
 
 #[shopify_function]
-fn cart_transform_run(_input: schema::cart_transform_run::CartTransformRunInput) -> Result<schema::CartTransformRunResult> {
+fn cart_transform_run(
+    _input: schema::cart_transform_run::CartTransformRunInput,
+) -> Result<schema::CartTransformRunResult> {
+    let _ = _input;
     let no_changes = schema::CartTransformRunResult { operations: vec![] };
 
     Ok(no_changes)
