ace50a30f06387601a3b27c48452f1933f55a093
Tanusri Harish <tanusri.harish@shopify.com>
2025-07-07 13:07:29 -0400

    updating cart transform CLI templates to 2025-07

---
+++ functions-cart-transform-rs/src/cart_transform_run.rs
@@ -0,0 +1,10 @@
+use super::schema;
+use shopify_function::prelude::*;
+use shopify_function::Result;
+
+#[shopify_function]
+fn cart_transform_run(_input: schema::cart_transform_run::CartTransformRunInput) -> Result<schema::CartTransformRunResult> {
+    let no_changes = schema::CartTransformRunResult { operations: vec![] };
+
+    Ok(no_changes)
+}
