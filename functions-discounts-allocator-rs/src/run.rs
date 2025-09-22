ba7c9eaafa881ccd1ff55d7a2f9ed37c60b03416
Sagarika Dasgupta <saga.dasgupta@shopify.com>
2025-09-22 15:13:17 -0500

    Removing function unit tests in favor of wasm level tests

--- functions-discounts-allocator-rs/src/run.rs
+++ functions-discounts-allocator-rs/src/run.rs
@@ -11,30 +11,3 @@ fn run(_input: schema::run::Input) -> Result<schema::FunctionRunResult> {
 
     Ok(no_discounts)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use shopify_function::run_function_with_input;
-
-    #[test]
-    fn test_result_with_no_discounts() -> Result<()> {
-        let result = run_function_with_input(
-            run,
-            r#"
-            {
-              "shop": {
-                "metafield": null
-              }
-            }
-            "#,
-        )?;
-        let expected = schema::FunctionRunResult {
-            line_discounts: Some(vec![]),
-            displayable_errors: Some(vec![]),
-        };
-
-        assert_eq!(result, expected);
-        Ok(())
-    }
-}
